7048de89b1ce0683638380bbcbf96dbbf9efd53b
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-04 11:07:48 -0500

    alpha 0.0.41

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -10,10 +10,10 @@ const NavBar = (props) => {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${not(props.dark)} bg-${not(props.dark)}`}>
         <div className={`container-fluid`}>
-          <NavBarBrand setDark={props.setDark} color={props.dark} />
           <button className={`navbar-toggler`} type={`button`} data-bs-toggle={`collapse`} data-bs-target={`#navbarNavDropdown`} aria-controls={`navbarNavDropdown`} aria-expanded={false} aria-label={`Toggle navigation`}>
             <span className={`navbar-toggler-icon`}></span>
           </button>
+          <NavBarBrand setDark={props.setDark} color={props.dark} />
           <NavBarLinkList links={["Home", "Benefits", "Contact Us", "Log In"]} />
         </div>
       </nav>
