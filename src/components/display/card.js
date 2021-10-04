471ee237bd3292c0be8a949de9c0375067160842
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-04 17:32:06 -0500

    alpha 0.0.425

--- src/components/display/card.js
+++ src/components/display/card.js
@@ -4,7 +4,7 @@ import {Link} from 'gatsby';
 const Card = (props) => {
     return (
         <div className={`card text-start rounded text-start`}>
-            <div className="card-header rounded-top text-light" style="background: var(--darkAccent);">
+            <div className="card-header rounded-top text-light" style={"background: var(--dark)"}>
                 {props.header}
             </div>
             <div className="card-body">
