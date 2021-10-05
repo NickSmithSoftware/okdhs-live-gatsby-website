19e99161309225be028dd9388394b7c775603ea0
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-05 09:59:03 -0500

    alpha 0.0.511

--- src/components/navigation/navbar/navbar-brand.js
+++ src/components/navigation/navbar/navbar-brand.js
@@ -7,7 +7,7 @@ const NavBarBrand = (props) => {
     <div className="pe-2 d-inline-flex align-items-center">
         <IconSwitch class="icon" onClick={props.setDark} iconClass="day" iconAltClass="night" />
         <Link to="/" className={`navbar-brand ms-2`}>
-                OKDHS-Live!
+                <h1>OKDHS-Live!</h1>
         </Link>
       </div>
     )
