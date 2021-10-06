6d97599942f32143d6eead1a042d7a165af7a036
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-06 15:11:22 -0500

    alpha 0.0.633

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Footer = (props) => {
     return (
-        <h4 className="bg-secondary text-center">Oklahoma Department of Human Services</h4>
+        <div className="text-center">
+            <p className="text-center">Oklahoma Department of Human Services</p>
+            <div className="text-center">
+                Mailing address: <br />
+                OKDHSLive! <br />
+                P.O. Box 2700 Norman, OK 73070 <br />
+                (405) 522 - 5050
+            </div>
+        </div>
     )
 }
 
