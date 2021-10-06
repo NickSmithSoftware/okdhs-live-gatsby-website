fb04f1f8f036ecf635d4d601c96987dda61ff10d
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-06 16:01:02 -0500

    alpha 0.0.67

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <div className="text-center footer">
-            <p className="text-center">Oklahoma Department of Human Services</p>
-            <div className="text-center">
-                Mailing address: <br />
-                OKDHSLive! <br />
-                P.O. Box 2700 Norman, OK 73070 <br />
-                (405) 522 - 5050
+            <div className="footer-card my-3 w-50">
+                <p className="text-center">Oklahoma Department of Human Services</p>
+                <div className="text-center">
+                    Mailing address: <br />
+                    OKDHSLive! <br />
+                    P.O. Box 2700 Norman, OK 73070 <br />
+                    (405) 522 - 5050
+                </div>
             </div>
         </div>
     )
