2715ee2243cbaa8e070059a992de72c96f871ba1
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-06 12:20:58 -0500

    alpha 0.0.617

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,30 +12,25 @@ const Index = () => {
 
         <div className="row h-25 pb-3">
 
-          <div className="col">
+          <div className="col-xs">
             <Card header="Account" title="Create an account." link="Register">
               Create an account to upload documents or do renewals, reviews, and applications. Accounts save eligibility assessments.
             </Card>
           </div>
 
-          <div className="col">
+          <div className="col-sm">
             <Card header="Benefits Eligibility" title="Find out if you're eligible." link="Eligibility Calculator">
               The OKDHSLive! web site is an easy way to see if you might be eligible for Food Benefits or SoonerCare Medicaid.
             </Card>
           </div>
 
-        </div>
-        
-
-        <div className="row h-25 pt-3">
-
-          <div className="col">
+          <div className="col-md">
             <Card header="View My Cases" title="View your current benefits." link="Log In">
               View the status of an application or renewal. Contact your case worker.
             </Card>
           </div>
 
-          <div className="col">
+          <div className="col-lg">
             <Card header="Client Survey" title="What do you think?" link="Survey">
               Tell us what you think about your experience with DHS.
             </Card>
