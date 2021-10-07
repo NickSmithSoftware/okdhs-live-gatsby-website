92e1ecdcf174d37465f80c9a6de92afb7a684483
NickSmithSoftware <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-06 20:37:36 -0500

    alpha 0.0.711

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const PageNotFound = (props) => {
+    return (
+        <Layout>
+            <h1>THIS LINK HAS NOT BEEN CREATED YET</h1>
+        </Layout>
+    )
+}
+
+export default PageNotFound;
