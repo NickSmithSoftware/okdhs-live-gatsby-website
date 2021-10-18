88a167543d3410ae8f9a35d6344b4c2da0f9df72
Nicolas Smith <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-18 15:43:00 -0500

    removed register temp.

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,81 +1,81 @@
-import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect} from 'react';
 
-import Layout from '../components/layout';
+// import Layout from '../components/layout';
 
-const hasEmptyField = (form) => {
-    for(key in form) {
-        if(key == "") return true;
-    }
-    return false;
-}
+// const hasEmptyField = (form) => {
+//     for(key in form) {
+//         if(key == "") return true;
+//     }
+//     return false;
+// }
 
-const passwordsMatch = (form) => {
-    if(form.password == "" || form.passwordConfirmation == "") return false;
-    return form.password == form.passwordConfirmation;
-}
+// const passwordsMatch = (form) => {
+//     if(form.password == "" || form.passwordConfirmation == "") return false;
+//     return form.password == form.passwordConfirmation;
+// }
 
-const handleRegistration = async (e, form, auth) => {
-    if(!hasEmptyField(form) && passwordsMatch(form)) {
+// const handleRegistration = async (e, form, auth) => {
+//     if(!hasEmptyField(form) && passwordsMatch(form)) {
         
-    }
-}
+//     }
+// }
 
-export const Register = () => {
-    const [form, updateForm] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        passwordConfirmation: ""
-    })
-    useEffect(() => {
+// export const Register = () => {
+//     const [form, updateForm] = useState({
+//         firstName: "",
+//         lastName: "",
+//         email: "",
+//         password: "",
+//         passwordConfirmation: ""
+//     })
+//     useEffect(() => {
 
-    }, [])
-    return (
-        <Layout>
-            <form onSubmit={handleRegistration(e, form)}>
-                <label for="register-first-name" className="form-label">First Name</label>
-                <input value={form.firstName} type="text" className="form-control" id="register-first-name" onChange={(e) => {
-                    updateForm({
-                        ...form,
-                        firstName: e.target.value
-                    })
-                }}/>
+//     }, [])
+//     return (
+//         <Layout>
+//             <form onSubmit={handleRegistration(e, form)}>
+//                 <label for="register-first-name" className="form-label">First Name</label>
+//                 <input value={form.firstName} type="text" className="form-control" id="register-first-name" onChange={(e) => {
+//                     updateForm({
+//                         ...form,
+//                         firstName: e.target.value
+//                     })
+//                 }}/>
                 
-                <label for="register-last-name" className="form-label">First Name</label>
-                <input value={form.lastName} type="text" className="form-control" id="register-last-name" onChange={(e) => {
-                    updateForm({
-                        ...form,
-                        lastName: e.target.value
-                    })
-                }} />
+//                 <label for="register-last-name" className="form-label">First Name</label>
+//                 <input value={form.lastName} type="text" className="form-control" id="register-last-name" onChange={(e) => {
+//                     updateForm({
+//                         ...form,
+//                         lastName: e.target.value
+//                     })
+//                 }} />
                 
-                <label for="register-email" className="form-label">First Name</label>
-                <input value={form.email} type="email" className="form-control" id="register-email" onChange={(e) => {
-                    updateForm({
-                        ...form,
-                        email: e.target.value
-                    })
-                }} />
+//                 <label for="register-email" className="form-label">First Name</label>
+//                 <input value={form.email} type="email" className="form-control" id="register-email" onChange={(e) => {
+//                     updateForm({
+//                         ...form,
+//                         email: e.target.value
+//                     })
+//                 }} />
                 
-                <label for="register-password" className="form-label">First Name</label>
-                <input value={form.password} type="password" className="form-control" id="register-password" onChange={(e) => {
-                    updateForm({
-                        ...form,
-                        password: e.target.value
-                    })
-                }} />
+//                 <label for="register-password" className="form-label">First Name</label>
+//                 <input value={form.password} type="password" className="form-control" id="register-password" onChange={(e) => {
+//                     updateForm({
+//                         ...form,
+//                         password: e.target.value
+//                     })
+//                 }} />
 
-                <label for="register-password-confirmation" className="form-label">First Name</label>
-                <input type="password" className="form-control" id="register-password-confirmation" onChange={(e) => {
-                    updateForm({
-                        ...form,
-                        passwordConfirmation: e.target.value
-                    })
-                }} />
+//                 <label for="register-password-confirmation" className="form-label">First Name</label>
+//                 <input type="password" className="form-control" id="register-password-confirmation" onChange={(e) => {
+//                     updateForm({
+//                         ...form,
+//                         passwordConfirmation: e.target.value
+//                     })
+//                 }} />
 
-                <button type="submit" className="btn btn-primary" >Submit</button>
-            </form>
-        </Layout>
-    )
-}
+//                 <button type="submit" className="btn btn-primary" >Submit</button>
+//             </form>
+//         </Layout>
+//     )
+// }
