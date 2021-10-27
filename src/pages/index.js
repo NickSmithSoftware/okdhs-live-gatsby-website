37aa10e65983affb5145f38f836baba9e4f96933
Nicolas Smith <62912470+NickSmithSoftware@users.noreply.github.com>
2021-10-27 10:46:25 -0500

    edited form links

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const Index = () => {
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 h-25">
 
           <div className="col my-3 cards">
-            <Card header="Account" title="Create an account." link="Form">
+            <Card header="Account" title="Create an account." link="form">
               Create an account to upload documents or do renewals, reviews, and applications. Accounts save eligibility assessments.
             </Card>
           </div>
@@ -37,7 +37,7 @@ const Index = () => {
           </div>
 
           <div className="col my-3 cards">
-            <Card header="Temp Form Card" title="Form" link="Form">
+            <Card header="Temp Form Card" title="Form" link="form">
               Link to form testing.
             </Card>
           </div>
